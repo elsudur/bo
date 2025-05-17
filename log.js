f458108e5d8133e559761db4f59635cc823da32b
elsudur <168886141+elsudur@users.noreply.github.com>
2025-05-17 21:47:41 +0700

    Update log.js

--- log.js
+++ log.js
@@ -1,4 +1,11 @@
 <script>
-  console.log('script loaded')
-  const avatar = 'my Avatar';
+  console.log('script loaded');
+  const helloOne = 'my Avatar 1';
+  let helloTwo = 'my Avatar 2';
+  var helloTree = 'my Avatar 3';
+  alert('script loaded');
+function tttt(name){
+  console.log(name);
+}
+tttt('LSDR')
 </script>
